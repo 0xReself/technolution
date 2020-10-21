2e624269610b1e718d0f2d00b8fd299f142433f1
Lars Schneider <lars.m.schneider2002@gmail.com>
2020-10-21 14:54:26 +0200

    init mod

---
+++ src/main/java/com/github/technolution/technolution/Technolution.java
@@ -0,0 +1,17 @@
+package com.github.technolution.technolution;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Technolution.ModID)
+public class Technolution {
+    public static final String ModID = "technolution";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Technolution() {
+        LOGGER.debug(ModID + " is starting ...");
+    }
+}
