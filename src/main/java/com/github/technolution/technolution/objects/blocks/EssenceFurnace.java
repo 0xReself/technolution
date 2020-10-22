838c8e286eeba00e8959d30b96f37943c1b3e413
Lars Schneider <lars.m.schneider2002@gmail.com>
2020-10-23 00:02:35 +0200

    fixing furnace can hold fluids like a tank

--- src/main/java/com/github/technolution/technolution/objects/blocks/EssenceFurnace.java
+++ src/main/java/com/github/technolution/technolution/objects/blocks/EssenceFurnace.java
@@ -5,18 +5,81 @@ import com.github.technolution.technolution.objects.tileentity.EssenceFurnaceEnt
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 
 public class EssenceFurnace extends Block {
 
     public EssenceFurnace() {
         super(Block.Properties.create(Material.ROCK));
     }
-    
+
     @Override
     public boolean hasTileEntity(BlockState state) {
         return true;
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        ItemStack stack = player.getHeldItem(handIn);
+
+        if(worldIn.isRemote()) {
+            return ActionResultType.CONSUME;
+        }
+
+        if(stack.getItem() == Items.AIR) {
+            return ActionResultType.CONSUME;
+        }
+
+        EssenceFurnaceEntity ent = (EssenceFurnaceEntity) worldIn.getTileEntity(pos);
+        if(stack.getItem() instanceof BucketItem) {
+            BucketItem bucket = (BucketItem) stack.getItem();
+            if(stack.getItem() == Items.BUCKET) {
+                if(ent.getTank().getFluidAmount() >= 1000) {
+                    if(!player.isCreative()) {
+                        if(stack.getCount() > 1) {
+                            stack.shrink(1);
+                            player.addItemStackToInventory(FluidUtil.getFilledBucket(ent.getTank().getFluid()));
+                        } else {
+                            player.setHeldItem(handIn, FluidUtil.getFilledBucket(ent.getTank().getFluid()));
+                        }
+                    }
+                    ent.getTank().drain(1000, IFluidHandler.FluidAction.EXECUTE);
+                    worldIn.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    player.sendMessage(new TranslationTextComponent("Tank: " + ent.getTank().getFluidAmount() + " / " + ent.getTank().getCapacity()));
+                    return ActionResultType.SUCCESS;
+                }
+            } else {
+                if(bucket.getFluid() == ent.getTank().getFluid().getFluid() || ent.getTank().isEmpty()) {
+                    if(ent.getTank().getTankCapacity(0) - ent.getTank().getFluidAmount() >= 1000) {
+                        ent.getTank().fill(new FluidStack(bucket.getFluid(), 1000), IFluidHandler.FluidAction.EXECUTE);
+                        if(!player.isCreative()) {
+                            player.setHeldItem(handIn, new ItemStack(Items.BUCKET));
+                        }
+                        worldIn.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                        player.sendMessage(new TranslationTextComponent("Tank: " + ent.getTank().getFluidAmount() + " / " + ent.getTank().getCapacity()));
+                        return ActionResultType.SUCCESS;
+                    }
+                }
+            }
+        }
+        player.sendMessage(new TranslationTextComponent("Tank: " + ent.getTank().getFluidAmount() + " / " + ent.getTank().getCapacity()));
+        return ActionResultType.CONSUME;
     }
 
     @Override
--- src/main/java/com/github/technolution/technolution/objects/tileentity/EssenceFurnaceEntity.java
+++ src/main/java/com/github/technolution/technolution/objects/tileentity/EssenceFurnaceEntity.java
@@ -2,6 +2,7 @@ package com.github.technolution.technolution.objects.tileentity;
 
 import com.github.technolution.technolution.init.Register;
 
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
@@ -10,6 +11,10 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
@@ -19,60 +24,74 @@ public class EssenceFurnaceEntity extends TileEntity implements ITickableTileEnt
     public EssenceFurnaceEntity() {
         super(Register.ESSENCE_FURNACE_ENTITY.get());
     }
-    
+
     @Override
     public void tick() {
-        //itemHandler.setStackInSlot(0, new ItemStack(Items.DIAMOND, 2));
+        // itemHandler.setStackInSlot(0, new ItemStack(Items.DIAMOND, 2));
     }
 
-    private ItemStackHandler itemHandler = createHandler();
-
-    private LazyOptional<IItemHandler> handler = LazyOptional.of(() -> itemHandler);
-
-    private ItemStackHandler createHandler() {
-        return new ItemStackHandler(1) {
-            @Override
-            protected void onContentsChanged(int slot) {
-                markDirty();
+    private FluidTank fluidHandler = new FluidTank(5000) {
+        @Override
+        public boolean isFluidValid(int tank, FluidStack stack) {
+            if(stack.getFluid() == Fluids.WATER) {
+                return true;
             }
-              
-            @Override
-            public boolean isItemValid(int slot, ItemStack stack)
-            {
-                if(stack.getItem() == Items.DIAMOND) {
-                    return true;
-                }
-                return false;
-            }
+            return false;
+        }
+    };
 
-            @Override
-            public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-                if(stack.getItem() != Items.DIAMOND) {
-                    return stack;
-                }
-                return super.insertItem(slot, stack, simulate);
-            }
-        };
+    public FluidTank getTank() {
+        return fluidHandler;
     }
+
+    private ItemStackHandler itemHandler = new ItemStackHandler(1) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            markDirty();
+        }
+          
+        @Override
+        public boolean isItemValid(int slot, ItemStack stack)
+        {
+            if(stack.getItem() == Items.DIAMOND) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+            if(stack.getItem() != Items.DIAMOND) {
+                return stack;
+            }
+            return super.insertItem(slot, stack, simulate);
+        }
+    };
+
+    private LazyOptional<IItemHandler> itemHandlerCap = LazyOptional.of(() -> itemHandler);
+    private LazyOptional<IFluidHandler> fluidHandlerCap = LazyOptional.of(() -> fluidHandler);
 
     @Override
     public void read(CompoundNBT tag) {
         itemHandler.deserializeNBT(tag.getCompound("inventory"));
-
+        fluidHandler.readFromNBT(tag);
         super.read(tag);
     }
 
     @Override
     public CompoundNBT write(CompoundNBT tag) {
         tag.put("inventory", itemHandler.serializeNBT());
-
+        fluidHandler.writeToNBT(tag);
         return super.write(tag);
     }
 
     @Override
     public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
         if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return handler.cast();
+            return itemHandlerCap.cast();
+        }
+        if(cap == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return fluidHandlerCap.cast();
         }
         return super.getCapability(cap, side);
     }
